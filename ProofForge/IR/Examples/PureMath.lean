e245b0e633792151bb6f855395221f464cc5aee2
Davirain <davirain.yin@gmail.com>
2026-07-02 05:32:07 +0800

    feat(ir): add PureMath example for Leo control flow

---
+++ ProofForge/IR/Examples/PureMath.lean
@@ -0,0 +1,51 @@
+import ProofForge.IR.Contract
+
+namespace ProofForge.IR.Examples.PureMath
+
+open ProofForge.IR
+
+def add : Entrypoint := {
+  name := "add"
+  params := #[("a", .u64), ("b", .u64)]
+  returns := .u64
+  body := #[ .return (.add (.local "a") (.local "b")) ]
+}
+
+def max : Entrypoint := {
+  name := "max"
+  params := #[("a", .u64), ("b", .u64)]
+  returns := .u64
+  body := #[
+    .ifElse (.gt (.local "a") (.local "b"))
+      #[ .return (.local "a") ]
+      #[ .return (.local "b") ]
+  ]
+}
+
+def sumFirst10 : Entrypoint := {
+  name := "sumFirst10"
+  params := #[]
+  returns := .u64
+  body := #[
+    .letBind "total" .u64 (.literal (.u64 0)),
+    .boundedFor "i" 0 10 #[
+      .assign (.local "total") (.add (.local "total") (.local "i"))
+    ],
+    .return (.local "total")
+  ]
+}
+
+def isEven : Entrypoint := {
+  name := "isEven"
+  params := #[("n", .u64)]
+  returns := .bool
+  body := #[ .return (.eq (.mod (.local "n") (.literal (.u64 2))) (.literal (.u64 0))) ]
+}
+
+def module : Module := {
+  name := "PureMath"
+  state := #[]
+  entrypoints := #[add, max, sumFirst10, isEven]
+}
+
+end ProofForge.IR.Examples.PureMath
